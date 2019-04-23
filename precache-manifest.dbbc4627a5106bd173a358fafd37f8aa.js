9953f60cccc043f8f577e484e89283a99ed8115c
svyatoslav00012 <svyatoslav.shilkov.2012@gmail.com>
2019-04-23 13:10:07 +0300

    Updates

---
+++ precache-manifest.dbbc4627a5106bd173a358fafd37f8aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f220230c18e3ba6f0dd",
+    "url": "/ReactFaceCapture/static/js/runtime~main.8f220230.js"
+  },
+  {
+    "revision": "a155d69d16735c1bae54",
+    "url": "/ReactFaceCapture/static/js/main.a155d69d.chunk.js"
+  },
+  {
+    "revision": "03c39368fbb8959f175a",
+    "url": "/ReactFaceCapture/static/js/1.03c39368.chunk.js"
+  },
+  {
+    "revision": "a155d69d16735c1bae54",
+    "url": "/ReactFaceCapture/static/css/main.22b617f0.chunk.css"
+  },
+  {
+    "revision": "dd20060a2f85942d8d8bca7c9299d3f7",
+    "url": "/ReactFaceCapture/index.html"
+  }
+];
